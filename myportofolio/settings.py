79de916fea9366885d86e00712bb6b70364ba8a4
Nanda Athaillah Nurano <nanda.athaillah@ui.ac.id>
2026-09-02 15:50:06 +0700

    Additional

--- myportofolio/settings.py
+++ myportofolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-n&10pw5!+_z(3t$pz+&5)n%i*3o0%6a16@+_t3(tk!eloqnydm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","nanda.athaillah-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","nanda.athaillah-myportofolio.pws.cs.ui.ac.id","nanda-athaillah-myportofolio.pws.cs.ui.ac.id"]
 
 
 # Application definition
