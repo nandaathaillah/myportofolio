e30b35efc3e5434acc7f5ca61790f6f090eb402d
Nanda Athaillah Nurano <nanda.athaillah@ui.ac.id>
2026-09-02 15:37:39 +0700

    First upload : -Settings.py -urls.py -views.py -style.css -static folder
    Signed-off-by: Nanda Athaillah Nurano <nanda.athaillah@ui.ac.id>

--- myportofolio/urls.py
+++ myportofolio/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myportofolio.views import landing_page
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing_page'),
+
 ]
